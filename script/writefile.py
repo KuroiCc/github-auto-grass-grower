38bdf4189f63b6f7d59d04927c4410b1c867fe40
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-12 17:49:07 +0900

    write date

--- script/writefile.py
+++ script/writefile.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    text = datetime.now().strftime("%Y-%m-%d %X") + "aaa\n"
+    text = datetime.now().strftime("%Y-%m-%d %X") + "\n"
 
     with open(path, "a") as file:
         file.write(text)
