682f71485210c59d538e781001d72427a82c634c
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-15 22:18:13 +0900

    human like

--- scripts/scripts/auto_commit.sh
+++ scripts/scripts/auto_commit.sh
@@ -1,6 +1,6 @@
 cd /repo
 git pull
-python3 /scripts/writefile.py "/repo/${records_file_path:-"records.txt"}"
+python3 /scripts/writefile.py "/repo/${records_file_path:-"records.txt"}" "${human_like:-0}"
 git add .
 git commit -m "${commit_mesg:-"auto commit"}"
 git push
