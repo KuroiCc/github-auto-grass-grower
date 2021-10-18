4a3f524ee6cfd4ec7d3a5e287b602cbc30975e46
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-10-18 15:49:57 +0900

    emv email is necessary and remove unnecessary cron tasks

--- scripts/scripts/start.sh
+++ scripts/scripts/start.sh
@@ -1,7 +1,7 @@
 git config --global user.name ${git_name:-"docker-github-auto-grass-grower"}
-git config --global user.email ${email:-"noemail"}
+git config --global user.email ${email:?"email has not been set"}
 git clone ${repo_url:?"repo url has not been set"} repo
 echo "init git done."
-sed -i "$ c${gpu_schedule:-"0 12 * * *"} /bin/sh /scripts/auto_commit.sh" var/spool/cron/crontabs/root
+echo "${gpu_schedule:-"0 12 * * *"} /bin/sh /scripts/auto_commit.sh" > /var/spool/cron/crontabs/root
 crond -b
 sh /scripts/auto_commit.sh
